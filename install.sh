213725e58bbe774e2d9c36cfc84f81cac4acf1f0
daffi1238 <jazz.sumariva@gmail.com>
2022-11-13 15:12:31 -0500

    tmux configuration

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install xclip
+
+##docker
+sudo apt install -y docker.io
+sudo systemctl enable docker --now
+sudo usermod -aG docker $USER
+printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+sudo apt install docker-compose
