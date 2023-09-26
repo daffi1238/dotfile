0089b9c713f48a428305b9394e737b6f1e888553
daffi1238 <jazz.sumariva@gmail.com>
2023-09-26 18:41:24 -0400

    smb enumeration

--- tools/template_loop.sh
+++ tools/template_loop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Función para manejar Ctrl+C
+ctrl_c() {
+  echo -e "\nScript interrumpido por el usuario."
+  exit 1
+}
+
+# Configurar la trampa para Ctrl+C (SIGINT)
+trap 'ctrl_c' SIGINT
+
 if [[ ! -f "$1" ]]; then
   echo "Por favor, proporciona un archivo válido."
   exit 1
